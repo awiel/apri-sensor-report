387bf22a739b65de11168e0331818f6d587735f0
awiel <awiel@scapeler.com>
2025-02-12 13:30:00 +0100

    knmi hist

--- apri-sensor-knmi.R
+++ apri-sensor-knmi.R
@@ -1,4 +1,7 @@
 options(warn=-1)
+
+# example: Rscript apri-sensor-knmi.R 06323 "Wilhelminadorp" 2025-02-09T00:00:00 2025-02-10T00:00:00
+
 ## Sys.setenv(TZ="Europe/Amsterdam")
 #print(sessionInfo())
 #print(version)
@@ -45,17 +48,34 @@ print(args)
 
 parmStation <- args[1]
 parmStationName <- args[2]
+argDateFrom <- args[3]
+argDateTo <- args[4]
 #parmStation <- '06323'
 #parmStationName <-'test'
 keeps <- c("sensorId","dateObserved","sensorValue")
+actualGraph<-TRUE
 
-fiwareStart<-format(Sys.time()-((2+24+hours)*60*60+(60*20)),"%Y-%m-%dT%H:%M:%S") # twee meer terug als start plus 20 minuten extra
-fiwareEnd<-format(Sys.time()-((hours)*60*60),"%Y-%m-%dT%H:%M:%S")
-yesterdayStart<-format(Sys.time()-((24+2+24+hours)*60*60+(60*20)),"%Y-%m-%dT%H:%M:%S") # twee meer terug als start plus 20 minuten extra
-yesterdayEnd<-format(Sys.time()-((2+24+hours)*60*60),"%Y-%m-%dT%H:%M:%S")
-
-fiwarePeriodStart<-format(Sys.time()-((12+hours)*60*60),"%Y-%m-%dT%H:%M:%S")
-fiwarePeriodEnd<-format(Sys.time()-((hours)*60*60),"%Y-%m-%dT%H:%M:%S")
+print(argDateFrom)
+print(argDateTo)
+if (is.na(argDateFrom) || is.na(argDateTo) ) {
+  actualGraph<-TRUE
+  fiwareStart<-format(Sys.time()-((2+24+hours)*60*60+(60*20)),"%Y-%m-%dT%H:%M:%S") # twee meer terug als start plus 20 minuten extra
+  fiwareEnd<-format(Sys.time()-((hours)*60*60),"%Y-%m-%dT%H:%M:%S")
+  yesterdayStart<-format(Sys.time()-((24+2+24+hours)*60*60+(60*20)),"%Y-%m-%dT%H:%M:%S") # twee meer terug als start plus 20 minuten extra
+  yesterdayEnd<-format(Sys.time()-((2+24+hours)*60*60),"%Y-%m-%dT%H:%M:%S")
+  
+  fiwarePeriodStart<-format(Sys.time()-((12+hours)*60*60),"%Y-%m-%dT%H:%M:%S")
+  fiwarePeriodEnd<-format(Sys.time()-((hours)*60*60),"%Y-%m-%dT%H:%M:%S")
+} else {
+  actualGraph<-FALSE
+  fiwareStart<-as.POSIXct(argDateFrom, format="%Y-%m-%dT%H:%M")
+  fiwareEnd<-as.POSIXct(argDateTo, format="%Y-%m-%dT%H:%M")
+  #yesterdayStart<-format(Sys.time()-((24+2+24+hours)*60*60+(60*20)),"%Y-%m-%dT%H:%M:%S") # twee meer terug als start plus 20 minuten extra
+  #yesterdayEnd<-format(Sys.time()-((2+24+hours)*60*60),"%Y-%m-%dT%H:%M:%S")
+  
+  fiwarePeriodStart<-format(fiwareStart,"%Y-%m-%dT%H:%M:%S")
+  # fiwarePeriodEnd<-format(fiwareEnd,"%Y-%m-%dT%H:%M:%S")
+}
 
 #fiwareStart='2022-02-27T00:00:00'
 #fiwareEnd='2022-02-27T22:00:00'
@@ -63,50 +83,54 @@ fiwarePeriodEnd<-format(Sys.time()-((hours)*60*60),"%Y-%m-%dT%H:%M:%S")
 #yesterdayEnd='2022-02-26T22:00:00'
 #dfTmpWind<-fiwareGetSensorSelectRecordsKnmi(NULL,'knmi','/knmi','station',parmStation,'windSpeed,windDirection',opPerRow='false',dateFrom=fiwareStart,dateTo=fiwareEnd)
 dfTmpWind<-getStationSelectRecordsKnmi(NULL,parmStation,dateFrom=fiwareStart,dateTo=fiwareEnd)
-dfTmpWindY<-getStationSelectRecordsKnmi(NULL,parmStation,dateFrom=yesterdayStart,dateTo=yesterdayEnd)
 dfTmpWind$date <- as.POSIXct(dfTmpWind$dateObserved, format="%Y-%m-%dT%H:%M")+ (as.numeric(format(Sys.time(),'%z'))/100)*60*60;
-dfTmpWindY$date <- as.POSIXct(dfTmpWindY$dateObserved, format="%Y-%m-%dT%H:%M")+ (as.numeric(format(Sys.time(),'%z'))/100)*60*60 + (24*60*60) ;
+if (actualGraph==TRUE) {
+  dfTmpWindY<-getStationSelectRecordsKnmi(NULL,parmStation,dateFrom=yesterdayStart,dateTo=yesterdayEnd)
+  dfTmpWindY$date <- as.POSIXct(dfTmpWindY$dateObserved, format="%Y-%m-%dT%H:%M")+ (as.numeric(format(Sys.time(),'%z'))/100)*60*60 + (24*60*60) ;
+}
 
 if (parmStation=="06225") { # merge two stations
   keeps<-c('station','dateObserved','date','name','wd','ws')
   keeps_06257<-c('date','rainfall','pressure','rainDuration','temperature','rHum', 'solar')
   dfTmpWind_06257<-getStationSelectRecordsKnmi(NULL,'06257',dateFrom=fiwareStart,dateTo=fiwareEnd)
-  dfTmpWindY_06257<-getStationSelectRecordsKnmi(NULL,'06257',dateFrom=yesterdayStart,dateTo=yesterdayEnd)
   dfTmpWind_06257$date <- as.POSIXct(dfTmpWind_06257$dateObserved, format="%Y-%m-%dT%H:%M")+ (as.numeric(format(Sys.time(),'%z'))/100)*60*60;
-  dfTmpWindY_06257$date <- as.POSIXct(dfTmpWindY_06257$dateObserved, format="%Y-%m-%dT%H:%M")+ (as.numeric(format(Sys.time(),'%z'))/100)*60*60 + (24*60*60) ;
   dfTmpWind_06257<-dfTmpWind_06257[keeps_06257]
-  dfTmpWindY_06257<-dfTmpWindY_06257[keeps_06257]
   dfTmpWind<-dfTmpWind[keeps]
-  dfTmpWindY<-dfTmpWindY[keeps]
   dfTmpWind<-merge(dfTmpWind,dfTmpWind_06257, by= c('date'),all.x=TRUE,all.y=T,sort=T)
-  dfTmpWindY<-merge(dfTmpWindY,dfTmpWindY_06257, by= c('date'),all.x=TRUE,all.y=T,sort=T)
+  if (actualGraph==TRUE) {
+    dfTmpWindY_06257<-getStationSelectRecordsKnmi(NULL,'06257',dateFrom=yesterdayStart,dateTo=yesterdayEnd)
+    dfTmpWindY_06257$date <- as.POSIXct(dfTmpWindY_06257$dateObserved, format="%Y-%m-%dT%H:%M")+ (as.numeric(format(Sys.time(),'%z'))/100)*60*60 + (24*60*60) ;
+    dfTmpWindY_06257<-dfTmpWindY_06257[keeps_06257]
+    dfTmpWindY<-dfTmpWindY[keeps]
+    dfTmpWindY<-merge(dfTmpWindY,dfTmpWindY_06257, by= c('date'),all.x=TRUE,all.y=T,sort=T)
+  } 
 }
 
-# begrenzing op max. 24 uur terug in de tijd
-ymdHM <- format(Sys.time() - as.difftime(1, unit="days"), "%y%m%d%H%M");
-# overrule e.g.
-#ymdHM <- "1809161100"
-
 total0 <- dfTmpWind
-total0Y <- dfTmpWindY
 
-total <- subset(total0, ( !is.na(total0$ws) & format(total0$date, "%y%m%d%H%M") >= ymdHM) )
-totalRain <- subset(total0, (!is.na(total0$rainfall) & format(total0$date, "%y%m%d%H%M") >= ymdHM))
-totalTemperature <- subset(total0,( !is.na(total0$temperature) &  format(total0$date, "%y%m%d%H%M") >= ymdHM))
-totalTemperatureY <- subset(total0Y,( !is.na(total0Y$temperature) &  format(total0Y$date, "%y%m%d%H%M") >= ymdHM))
-totalRelativeHumidity <- subset(total0,( !is.na(total0$rHum) &  format(total0$date, "%y%m%d%H%M") >= ymdHM))
-totalRelativeHumidityY <- subset(total0Y,( !is.na(total0Y$rHum) &  format(total0Y$date, "%y%m%d%H%M") >= ymdHM))
-totalSolar <- subset(total0,( !is.na(total0$solar) &  format(total0$date, "%y%m%d%H%M") >= ymdHM))
-totalSolarY <- subset(total0Y,( !is.na(total0Y$solar) &  format(total0Y$date, "%y%m%d%H%M") >= ymdHM))
-#print(head(totalRain))
-#totalTemperature <- subset(dfTmpTemperature, format(dfTmpTemperature$date, "%y%m%d%H%M") >= ymdHM)
-#head(dfTmpTemperature)
-#head(dfTmpTemperatureY)
-#totalTemperatureY <- subset(dfTmpTemperatureY, format(dfTmpTemperatureY$date, "%y%m%d%H%M") >= ymdHM)
-#totalRelativeHumidity <- subset(dfTmpRelativeHumidity, format(dfTmpRelativeHumidity$date, "%y%m%d%H%M") >= ymdHM)
-#totalRelativeHumidityY <- subset(dfTmpRelativeHumidityY, format(dfTmpRelativeHumidityY$date, "%y%m%d%H%M") >= ymdHM)
-#totalSolar <- subset(dfTmpSolar, format(dfTmpSolar$date, "%y%m%d%H%M") >= ymdHM)
-#totalSolarY <- subset(dfTmpSolarY, format(dfTmpSolarY$date, "%y%m%d%H%M") >= ymdHM)
+if (actualGraph==TRUE) {
+  # begrenzing op max. 24 uur terug in de tijd
+  ymdHM <- format(Sys.time() - as.difftime(1, unit="days"), "%y%m%d%H%M");
+  # overrule e.g.
+  #ymdHM <- "1809161100"
+  
+  total0Y <- dfTmpWindY
+  
+  total <- subset(total0, ( !is.na(total0$ws) & format(total0$date, "%y%m%d%H%M") >= ymdHM) )
+  totalRain <- subset(total0, (!is.na(total0$rainfall) & format(total0$date, "%y%m%d%H%M") >= ymdHM))
+  totalTemperature <- subset(total0,( !is.na(total0$temperature) &  format(total0$date, "%y%m%d%H%M") >= ymdHM))
+  totalTemperatureY <- subset(total0Y,( !is.na(total0Y$temperature) &  format(total0Y$date, "%y%m%d%H%M") >= ymdHM))
+  totalRelativeHumidity <- subset(total0,( !is.na(total0$rHum) &  format(total0$date, "%y%m%d%H%M") >= ymdHM))
+  totalRelativeHumidityY <- subset(total0Y,( !is.na(total0Y$rHum) &  format(total0Y$date, "%y%m%d%H%M") >= ymdHM))
+  totalSolar <- subset(total0,( !is.na(total0$solar) &  format(total0$date, "%y%m%d%H%M") >= ymdHM))
+  totalSolarY <- subset(total0Y,( !is.na(total0Y$solar) &  format(total0Y$date, "%y%m%d%H%M") >= ymdHM))
+} else {
+  total <- subset(total0, ( !is.na(total0$ws) ) )
+  totalRain <- subset(total0, (!is.na(total0$rainfall) ))
+  totalTemperature <- subset(total0,( !is.na(total0$temperature) ))
+  totalRelativeHumidity <- subset(total0,( !is.na(total0$rHum) ))
+  totalSolar <- subset(total0,( !is.na(total0$solar) ))
+}
 
 total$angle<-(270-as.numeric(total$wd))*pi/180
 total$wforce <- as.numeric(total$ws)
@@ -147,17 +171,19 @@ if(nrow(totalTemperature)>0) {
  par(new = T)
  minT=min(totalTemperature$temperature)
  maxT=max(totalTemperature$temperature)
- minTY=min(totalTemperatureY$temperature)
- maxTY=max(totalTemperatureY$temperature)
  minTAll=minT
- if (minTY<minTAll) minTAll <- minTY
  maxTAll=maxT
- if (maxTY>maxTAll) maxTAll <- maxTY
+ if (actualGraph==TRUE) {
+   minTY=min(totalTemperatureY$temperature)
+   maxTY=max(totalTemperatureY$temperature)
+   if (minTY<minTAll) minTAll <- minTY
+   if (maxTY>maxTAll) maxTAll <- maxTY
+ }
 
  scalePosXTick=(max(totalRelativeHumidity$date)-min(totalRelativeHumidity$date))/100
  scalePosX=max(totalRelativeHumidity$date)+scalePosXTick*4
  plot(totalTemperature$date,totalTemperature$temperature,col="red", lwd=3, axes=F, xlab=NA, ylab=NA, type="l",ylim=c(minTAll,maxTAll))
- lines(totalTemperatureY$date,totalTemperatureY$temperature,col="red", lwd=1, lty=2, axis=F, xlab=NA, ylab=NA)
+ if (actualGraph==TRUE) lines(totalTemperatureY$date,totalTemperatureY$temperature,col="red", lwd=1, lty=2, axis=F, xlab=NA, ylab=NA)
  abline(h=40.4,col="blue")
  axis(4, at=c(-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,40), col = "red", col.axis = "dark red", las=1, lwd = 1,outer=FALSE, pos=scalePosX+scalePosXTick*0.90)
 
@@ -167,15 +193,17 @@ if(nrow(totalRelativeHumidity)>0) {
  par(new = T)
  minT=min(totalRelativeHumidity$rHum)
  maxT=max(totalRelativeHumidity$rHum)
- minTY=min(totalRelativeHumidityY$rHum)
- maxTY=max(totalRelativeHumidityY$rHum)
  minTAll=minT
- if (minTY<minTAll) minTAll <- minTY
  maxTAll=maxT
- if (maxTY>maxTAll) maxTAll <- maxTY
-
+ if (actualGraph==TRUE) {
+   minTY=min(totalRelativeHumidityY$rHum)
+   maxTY=max(totalRelativeHumidityY$rHum)
+   if (minTY<minTAll) minTAll <- minTY
+   if (maxTY>maxTAll) maxTAll <- maxTY
+ }
+ 
  plot(totalRelativeHumidity$date,totalRelativeHumidity$rHum,col="green", lwd=3, axes=F, xlab=NA, ylab=NA, type="l",ylim=c(minTAll,maxTAll))
- lines(totalRelativeHumidityY$date,totalRelativeHumidityY$rHum,col="green", lwd=1, lty=2, axis=F, xlab=NA, ylab=NA)
+ if (actualGraph==TRUE) lines(totalRelativeHumidityY$date,totalRelativeHumidityY$rHum,col="green", lwd=1, lty=2, axis=F, xlab=NA, ylab=NA)
  axis(4, at=c(0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100),col = "green", col.axis = "dark green", las=1, lwd = 1,outer=FALSE, pos=scalePosX+scalePosXTick*4.80)
 }
 
@@ -185,14 +213,22 @@ if(nrow(totalSolar>0)) {
  scalePosXTick=(max(totalSolar$date)-min(totalSolar$date))/100
  scalePosX=min(totalSolar$date)-scalePosXTick*8
  plot(totalSolar$date,totalSolar$solar,col="orange", lwd=3, axes=F, xlab=NA, ylab=NA, type="l")
- lines(totalSolarY$date,totalSolarY$solar,col="orange", lwd=1, lty=2, axis=F, xlab=NA, ylab=NA)
+ if (actualGraph==TRUE) lines(totalSolarY$date,totalSolarY$solar,col="orange", lwd=1, lty=2, axis=F, xlab=NA, ylab=NA)
  axis(2, at=c(0,50,100,150,200,250,300,350,400,450,500,550,600,650,700,750,800,850,900,950,1000),col = "orange", col.axis = "dark orange", las=1, lwd = 1,outer=FALSE, pos=scalePosX) #+scalePosXTick*4.20)
 }
 
-legend("bottomleft", legend=c("wind", "regen","zonnestraling"),
-       col=c("black", "blue","orange"), lwd=3, lty=1, cex=0.9)
-legend("bottomright", legend=c("temperature","temperature-24h","rel.Humidity","rel.Humidity-24h"),
-       col=c("red","red","green","green"), lwd=c(3,1,3,1), lty=c(1,2,1,2), cex=0.9)
+if (actualGraph==TRUE) {
+  legend("bottomleft", legend=c("wind", "regen","zonnestraling"),
+         col=c("black", "blue","orange"), lwd=3, lty=1, cex=0.9)
+  legend("bottomright", legend=c("temperature","temperature-24h","rel.Humidity","rel.Humidity-24h"),
+         col=c("red","red","green","green"), lwd=c(3,1,3,1), lty=c(1,2,1,2), cex=0.9)
+} else {
+  legend("bottomleft", legend=c("wind", "regen","zonnestraling"),
+         col=c("black", "blue","orange"), lwd=3, lty=1, cex=0.9)
+  legend("bottomright", legend=c("temperature","rel.Humidity"),
+         col=c("red","green"), lwd=c(3,3), lty=c(1,1), cex=0.9)
+  
+}
 
 mtext(side = 2, line=5, 'Wind (m/s), regen (mm) en zonnestraling (W/m2)')
 mtext(side = 4, line=5, 'temperatuur C en relatieve luchtvochtigheid %')
