c0c8c9faa5a295fa8b97052ee77c67f598e9131e
awiel <awiel@scapeler.com>
2025-10-12 23:59:27 +0200

    legend

--- sub/apri-sensor-plot.R
+++ sub/apri-sensor-plot.R
@@ -209,7 +209,7 @@ apriSensorPlotSingle<-function(dfTotal,dfFois,sensorTypes,foiLabel,foiText,ylim,
  #   print(dfIncidentStats)
   }
   
-  legendRows <- ceiling(length(unique(dfTotal$foiLocation))/4)
+  legendRows <- ceiling(length(unique(dfTotal$foiLocation))/3)
   gTotal <-ggplot(data=dfTotal, aes(x=date,y=sensorValue,colour=foiLocation,timezone=localTimezone)
                   ,col = brewer.pal(n = 8, name = "RdYlBu")) +
     guides(color = guide_legend(keywidth = 0.2, override.aes = list(size = 2))) +
